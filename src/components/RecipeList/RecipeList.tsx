c14d9dba99b55342bab249b284410c9efd0ce6ba
Diyana Mendoza <>
2022-02-23 15:08:55 -0800

    Add search bar to RecipeList component

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -3,24 +3,49 @@ import { getRecipes } from '../../utils/fetchUtils';
 
 export default function RecipeList() {
   const [recipes, setRecipes] = useState<any>([]);
+  const [searchResults, setSearchResults] = useState<any>([]);
+  const [searchInput, setSearchInput] = useState<any>([]);
+  const [list, setList] = useState(searchResults ? searchResults : recipes);
 
   useEffect(() => {
     const getAllRecipes = async () => {
       const res = await getRecipes();
       setRecipes(res);
+      setList(res);
     };
     getAllRecipes();
   }, []);
 
+  const searchItems = (query: string) => {
+    setSearchInput(query.toLowerCase());
+    const results = recipes.filter((recipe: any) => {
+      const name = recipe.name.toLowerCase();
+      return name.includes(query);
+    });
+    setSearchResults(results);
+    setList(results);
+  };
+
   return (
     <div>
       <h1>Recipes</h1>
-      {recipes.map((recipe: any) => {
+      <form>
+        <label htmlFor='search-items'></label>
+        <input
+          id='search-items'
+          type='text'
+          name='search-items'
+          value={searchInput}
+          autoComplete='off'
+          onChange={({ target }) => searchItems(target.value)}
+        />
+      </form>
+      {list.map((recipe: any) => {
         return (
-          <>
-            <h2 key={recipe.id}>{recipe.name}</h2>
+          <div key={recipe.id}>
+            <h2>{recipe.name}</h2>
             <p>{recipe.description}</p>
-          </>
+          </div>
         );
       })}
     </div>
