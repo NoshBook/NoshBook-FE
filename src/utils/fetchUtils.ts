bd4138782c840f03b3613030d835aa45435347a0
Michael Rider <mikepdxrider@gmail.com>
2022-02-24 15:02:37 -0800

    updates pagination hook and adds env.example

--- src/utils/fetchUtils.ts
+++ src/utils/fetchUtils.ts
@@ -1,7 +1,19 @@
 export const getRecipes = async () => {
   try {
+    const res = await fetch(`${process.env.RECIPE_URL}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getPaginatedRecipes = async (
+  newPage: number,
+  itemQuantity: number
+) => {
+  try {
     const res = await fetch(
-      'https://noshbook-staging.herokuapp.com/api/v1/recipes'
+      `${process.env.RECIPE_URL}?page=${newPage}&quantity=${itemQuantity}`
     );
     return await res.json();
   } catch (error) {
