9d1a4b1bc402b9c8fc14c54719f940b0ba5971c5
Diyana Mendoza <>
2022-02-25 15:17:58 -0800

    Add header w nav menu for mobile and desk

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,30 @@
+import { useAuth } from '../../context/AuthContext';
+import { logOut } from '../../utils/users';
+import { BsPersonCircle } from 'react-icons/bs';
+import { Navigate } from 'react-router-dom';
+
+export default function Avatar() {
+  const { user, setUser } = useAuth();
+
+  const handleLogout = async () => {
+    await logOut();
+    setUser('');
+    <Navigate to='/' />;
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <BsPersonCircle />
+          {user}{' '}
+          <button onClick={async () => await handleLogout()}>Logout</button>
+        </div>
+      ) : (
+        <a href='/auth' alt='log in'>
+          Login/Signup
+        </a>
+      )}
+    </div>
+  );
+}
