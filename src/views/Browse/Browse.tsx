6b8121760a8ece1efcce43e55c8739ab7e0e648b
Michael Rider <mikepdxrider@gmail.com>
2022-02-25 01:31:54 -0800

    adds basic functionality, tinkers with TS

--- src/views/Browse/Browse.tsx
+++ src/views/Browse/Browse.tsx
@@ -1,17 +1,24 @@
 import RecipeList from '../../components/RecipeList/RecipeList';
 import usePagination from '../../hooks/usePagination';
+import { insertRecipeIntoCookbook } from '../../services/cookbook';
 
 export default function Browse() {
   const { nextPage, prevPage, currentPageData, currentPage } =
     usePagination(20);
 
   // declare handleClick fn for recipe->cookbook
+  async function handleAddRecipeToCookbook(recipeId: string) {
+    await insertRecipeIntoCookbook(recipeId); // ❓ does success here trigger any events or user feedback?
+  }
 
   return (
     <main>
       <section>
         {/* pass new handleClick fn as prop to RecipeList */}
-        <RecipeList currentPageData={currentPageData} />
+        <RecipeList
+          currentPageData={currentPageData}
+          handleAddRecipeToCookbook={handleAddRecipeToCookbook}
+        />
       </section>
       <section>
         <button onClick={prevPage} disabled={currentPage === 1}>
