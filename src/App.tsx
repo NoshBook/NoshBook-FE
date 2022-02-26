2a922753a944974c7a62e42a5d663faed177efcc
Dylan Floyd <floyd800@gmail.com>
2022-02-25 16:36:57 -0800

    working shopping list, adding tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import { useAuth } from './context/AuthContext';
 import Auth from './views/Auth';
 import Browse from './views/Browse/Browse';
 import Planner from './views/Planner/Planner';
+import ShoppingListView from './views/ShoppingListView/ShoppingListView';
 
 function App() {
   /* including user console log below for dev purposes - remove later */
@@ -17,6 +19,11 @@ function App() {
         <Routes>
           <Route path='/auth' element={<Auth />} />
           <Route path='/planner' element={<Planner />} />
+          <Route path='/shoppinglist' element={
+            <PrivateRoute>
+              <ShoppingListView />
+            </PrivateRoute>
+          } />
           <Route path='/' element={<Browse />} />
         </Routes>
       </Router>
