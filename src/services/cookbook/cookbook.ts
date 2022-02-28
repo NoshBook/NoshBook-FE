e9d83d71711cfcb3281b28b94d074f6d30f0bf95
Michael Rider <mikepdxrider@gmail.com>
2022-02-27 17:16:34 -0800

    adds comments for necessary tests, adds try catch to cookbook services

--- src/services/cookbook/cookbook.ts
+++ src/services/cookbook/cookbook.ts
@@ -5,14 +5,18 @@ export async function insertRecipeIntoCookbook(
   recipeId: string,
   userId: string
 ) {
-  const res = await fetch(`${STAGING_URL}`, {
-    credentials: 'include',
-    method: 'POST',
-    body: JSON.stringify({ recipeId, userId }),
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${STAGING_URL}`, {
+      credentials: 'include',
+      method: 'POST',
+      body: JSON.stringify({ recipeId, userId }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
