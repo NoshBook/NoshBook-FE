fdfc77ced72928517bc79844d7527b350e9321bf
Michael Rider <mikepdxrider@gmail.com>
2022-02-27 19:36:17 -0800

    adds rating package/component

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { BrowseRecipe } from '../../interfaces/BrowseRecipe';
+import StarRatings from 'react-star-ratings';
 
 interface RecipeListProps {
   currentPageData: Array<any>;
@@ -28,21 +29,31 @@ export default function RecipeList({
           onChange={({ target }) => setSearchInput(target.value)}
         />
       </form>
-      {currentPageData.map((recipe: any) => {
-        return (
-          <div
-            key={recipe.id}
-            onClick={() => navigate(`/recipes/${recipe.id}`)}
-          >
-            <h2>{recipe.name}</h2>
-            <p>{recipe.description}</p>
-            {/* if rendering in cookbook view, this button should remove a recipe from the cookbook */}
-            <button onClick={() => handleAddRecipeToCookbook(recipe)}>
-              Add Recipe to Cookbook
-            </button>
-          </div>
-        );
-      })}
+      <ul>
+        {currentPageData.map((recipe: any) => {
+          return (
+            <li
+              key={recipe.id}
+              onClick={() => navigate(`/recipes/${recipe.id}`)}
+            >
+              <h2>{recipe.name}</h2>
+              <StarRatings
+                rating={recipe.rating}
+                starRatedColor="blue"
+                numberOfStars={5}
+                starDimension="15px"
+                starSpacing="2px"
+                name="rating"
+              />
+              <p>{recipe.description}</p>
+              {/* if rendering in cookbook view, this button should remove a recipe from the cookbook */}
+              <button onClick={() => handleAddRecipeToCookbook(recipe)}>
+                Add Recipe to Cookbook
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
