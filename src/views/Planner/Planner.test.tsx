e40e35ee7eb652909fe1472cf8e891fcdf3f8cfe
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-28 10:20:34 -0800

    Add clear recipes from planner feature with test

---
+++ src/views/Planner/Planner.test.tsx
@@ -0,0 +1,71 @@
+import { screen, render, findByText, fireEvent } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import Planner from './Planner';
+
+const server = setupServer(
+  rest.get(
+    'https://noshbook-staging.herokuapp.com/api/v1/planners',
+    (req, res, ctx) => {
+      const mockResponse = [
+        {
+          day: 'monday',
+          recipes: [
+            {
+              id: 1,
+              name: 'banana bread',
+            },
+            {
+              id: 2,
+              name: 'corndog',
+            },
+          ],
+        },
+      ];
+
+      return res(ctx.json(mockResponse));
+    }
+  ),
+  rest.delete(
+    'https://noshbook-staging.herokuapp.com/api/v1/planners/clear',
+    (req, res, ctx) => {
+      const mockResponse = [
+        {
+          day: 'monday',
+          recipes: [
+            {
+              id: 1,
+              name: 'banana bread',
+            },
+            {
+              id: 2,
+              name: 'corndog',
+            },
+          ],
+        },
+      ];
+
+      return res(ctx.json(mockResponse));
+    }
+  )
+);
+
+describe('PlannerList', () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should delete recipes from the planner on Reset click', async () => {
+    render(<Planner />);
+
+    await screen.findByText(/loading/i);
+    await screen.findByText(/banana bread/i);
+    const resetButton = screen.getByText(/reset/i);
+    fireEvent.click(resetButton);
+    expect(await screen.findByText(/no recipes to display/i)).toBeVisible();
+  });
+});
