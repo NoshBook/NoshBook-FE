1d7d2accac44276735313d7fe40ba3ff37f3e8c8
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-28 10:20:34 -0800

    Add clear recipes from planner feature with test

--- src/components/PlannerList/PlannerDay.tsx
+++ src/components/PlannerList/PlannerDay.tsx
@@ -1,10 +1,9 @@
+import { RecipesByDayType, RecipeType } from './plannerTypes';
+
 export default function PlannerDay({
   day,
   recipes,
-}: {
-  day: string;
-  recipes: any;
-}) {
+}: RecipesByDayType): JSX.Element {
   return (
     <div
       style={{
@@ -14,8 +13,12 @@ export default function PlannerDay({
       }}
     >
       <h1 style={{ textTransform: 'uppercase', color: 'coral' }}>{day}</h1>
-      {recipes.map((recipe: any) => {
-        return <h3 style={{ textDecoration: 'underline' }}>{recipe.name}</h3>;
+      {recipes.map((recipe: RecipeType, index: number): JSX.Element => {
+        return (
+          <h3 style={{ textDecoration: 'underline' }} key={index}>
+            {recipe.name}
+          </h3>
+        );
       })}
     </div>
   );
--- src/services/planner.ts
+++ src/services/planner.ts
@@ -1,9 +1,47 @@
-const PLANNER_URL = 'https://noshbook-staging.herokuapp.com/api/v1/planners';
+const URL = 'https://noshbook-staging.herokuapp.com/api/v1/planners';
 
 export const getPlannerRecipes = async () => {
   try {
-    const res = await fetch(PLANNER_URL, { credentials: 'include' });
+    const res = await fetch(URL, { credentials: 'include' });
     return await res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const clearPlannerRecipes = async () => {
+  try {
+    const res = await fetch(`${URL}/clear`, {
+      credentials: 'include',
+      method: 'DELETE',
+    });
+    return await res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const addPlannerRecipe = async ({
+  recipeId,
+  day,
+}: {
+  recipeId: number;
+  day: string;
+}) => {
+  try {
+    const res = await fetch(URL, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        recipeId,
+        day,
+      }),
+    });
+    return res.json();
   } catch (e) {
     console.log(e);
   }
