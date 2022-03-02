e00211213172002c2c7df517f2b34ed0d4b5f2cc
Michael Rider <mikepdxrider@gmail.com>
2022-03-02 15:39:56 -0800

    adds getPaginatedCookbookRecipes and updates usePagination hook for reusability

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,30 +1,42 @@
 /* eslint-disable */
 import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
+import { getPaginatedCookbookRecipes } from '../services/cookbook/cookbook';
 import { getPaginatedRecipes } from '../services/recipe';
+import { BrowseRecipe } from '../views/Browse/interfaces/BrowseRecipe';
 
 interface PaginationFeatures {
   nextPage: () => void;
   prevPage: () => void;
-  currentPageData: Array<any>;
+  currentPageData: Array<BrowseRecipe>;
   currentPage: number;
 }
 
 export default function usePagination(
   itemsPerPage: number,
+  isCookbookView?: boolean,
 ): PaginationFeatures {
   const [currentPage, setCurrentPage] = useState(1);
-  const [currentPageData, setCurrentPageData] = useState<Array<any>>([]);
+  const [currentPageData, setCurrentPageData] = useState<Array<BrowseRecipe>>(
+    [],
+  );
   const { user } = useAuth();
 
   useEffect(() => {
     async function fetchRecipes() {
-      const newPageData: any = await getPaginatedRecipes(
-        currentPage,
-        20,
-        user.showUserContent,
-      );
-      setCurrentPageData(newPageData);
+      // if rendering in cookbook view...
+      if (isCookbookView) {
+        const newPageData = await getPaginatedCookbookRecipes(currentPage, 20);
+        setCurrentPageData(newPageData);
+        // if rendering elsewhere(browse all)...
+      } else {
+        const newPageData = await getPaginatedRecipes(
+          currentPage,
+          20,
+          user.showUserContent,
+        );
+        setCurrentPageData(newPageData);
+      }
     }
     fetchRecipes();
   }, [currentPage, user.showUserContent]);
