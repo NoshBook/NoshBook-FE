aacce82d349afbf7caa767b1aca9ed3a54165295
Michael Rider <mikepdxrider@gmail.com>
2022-03-01 19:17:03 -0800

    makes recipe list component reusable, implements in cookbook

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable */
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import type { BrowseRecipe } from '../../views/Browse/interfaces/BrowseRecipe';
-import StarRatings from 'react-star-ratings';
-import { useAuth } from '../../context/AuthContext';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import { BrowseRecipe } from '../../views/Browse/interfaces/BrowseRecipe';
 
 interface RecipeListProps {
   currentPageData: Array<any>;
+  isCookbookView?: boolean;
+  plannerToggle?: boolean;
+  setPlannerToggle?: React.Dispatch<React.SetStateAction<boolean>>;
+  handleRemoveFromCookbookClick?: (id: string) => void;
+  handleAddToPlannerClick?: (day: string, recipeId: string) => void;
 }
 
-export default function RecipeList({ currentPageData }: RecipeListProps) {
+export default function RecipeList({
+  currentPageData,
+  isCookbookView,
+  handleRemoveFromCookbookClick,
+  handleAddToPlannerClick,
+}: RecipeListProps) {
   const [searchInput, setSearchInput] = useState('');
   const navigate = useNavigate();
 
@@ -35,7 +43,16 @@ export default function RecipeList({ currentPageData }: RecipeListProps) {
               key={recipe.id}
               onClick={() => navigate(`/recipes/${recipe.id}`)}
             >
-              <RecipeCard recipe={recipe} />
+              {isCookbookView ? (
+                <RecipeCard
+                  recipe={recipe}
+                  isCookbookView={true}
+                  handleRemoveFromCookbookClick={handleRemoveFromCookbookClick}
+                  handleAddToPlannerClick={handleAddToPlannerClick}
+                />
+              ) : (
+                <RecipeCard recipe={recipe} />
+              )}
             </li>
           );
         })}
