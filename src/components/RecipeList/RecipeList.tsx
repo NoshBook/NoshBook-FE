560b1480fcd57212123fde1f507d8867d8d4aa45
Dylan Floyd <floyd800@gmail.com>
2022-03-03 15:35:55 -0800

    hide search on cookbook page

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -34,19 +34,21 @@ export default function RecipeList({
   return (
     <div className={styles.container}>
       <h1>Recipes</h1>
-      <div className={styles.search}>
-        <div>
-          <label htmlFor="search-items"></label>
-          <input
-            id="search-items"
-            type="text"
-            name="search-items"
-            value={searchQuery}
-            autoComplete="off"
-            onChange={({ target }) => handleSearchInputChange(target.value)}
-          />
+      { !isCookbookView && (
+        <div className={styles.search}>
+          <div>
+            <label htmlFor="search-items"></label>
+            <input
+              id="search-items"
+              type="text"
+              name="search-items"
+              value={searchQuery}
+              autoComplete="off"
+              onChange={({ target }) => handleSearchInputChange(target.value)}
+            />
+          </div>
         </div>
-      </div>
+      )}
       <ul className={styles.listcontainer}>
         {currentPageData.map((recipe: any) => {
           const path = isCookbookView ? 'cookbook' : 'recipes';
