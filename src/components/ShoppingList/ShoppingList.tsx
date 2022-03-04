541651b726dfb651d327b0080da702a8d2750f36
Zack Lyon <zack.j.lyon@gmail.com>
2022-03-03 18:20:19 -0800

    Add FlipMove to ShoppingList and remove React.StrictMode from index.tsx

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -2,15 +2,21 @@ import ShoppingItem from './ShoppingItem';
 import { IShoppingItem, ShoppingListProps } from './shoppingListTypes';
 import styles from './ShoppingList.module.css';
 
+import FlipMove from 'react-flip-move';
+
 export default function ShoppingList({
   items = [],
   setChecked,
 }: ShoppingListProps) {
   return (
     <div className={styles.list}>
-      {items.map((item: IShoppingItem) => (
-        <ShoppingItem item={item} key={item.id} setChecked={setChecked} />
-      ))}
+      <FlipMove>
+        {items.map((item: IShoppingItem) => (
+          <div key={item.id}>
+            <ShoppingItem item={item} key={item.id} setChecked={setChecked} />
+          </div>
+        ))}
+      </FlipMove>
     </div>
   );
 }
